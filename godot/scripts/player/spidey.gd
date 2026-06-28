4aca9e72251b516bb4decb070b2e9a230488837c
Matthew McLeod <m@qwrk.dev>
2026-06-28 17:51:16 +0200

    chp

--- godot/scripts/player/spidey.gd
+++ godot/scripts/player/spidey.gd
@@ -20,7 +20,7 @@ func pick_new_state():
 		current_state = new_state
 		state_machine.travel(new_state)
 
-func _on_web_state_physics_processing(delta: float) -> void:
+func _on_web_state_physics_processing(_delta: float) -> void:
 	var keyboard_dir := Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
